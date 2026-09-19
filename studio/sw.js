7c77582bc54bdb3ed13ffcf756c587719c91fd34
henmario99-art <henmario99@gmail.com>
2026-09-19 15:52:22 -0600

    Cache local Fabric.js engine

--- studio/sw.js
+++ studio/sw.js
@@ -1,5 +1,5 @@
-const CACHE='haki-studio-v1';
-const LOCAL=['./','./index.html','./styles.css','./app.js','./core.js','./editor.js','./warp.js','./storage.js','./ui.js','./manifest.webmanifest','./icon.svg'];
+const CACHE='haki-studio-v2';
+const LOCAL=['./','./index.html','./styles.css','./app.js','./core.js','./editor.js','./warp.js','./storage.js','./ui.js','./manifest.webmanifest','./icon.svg','./vendor/fabric.min.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(LOCAL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
